4e3b74a43b5c4cc656bfd73b4edbd0a5a58f9342
Miguel Caro <miguelcaropinilla@gmail.com>
2024-10-25 16:40:52 -0500

    hardcode api url

--- react-demo/vite.config.js
+++ react-demo/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
+// import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const API_URL = `${env.VITE_API_URL}/api/v1`;
+// export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
+  // const env = loadEnv(mode, process.cwd());
+  const API_URL = 'http://a5c3b35c3017f43b293a28d0a9009202-106008128.us-east-1.elb.amazonaws.com';
 
   console.log("🚀 ~ defineConfig ~ API_URL:", API_URL)
-  
+
   return {
     server: {
       proxy: {
