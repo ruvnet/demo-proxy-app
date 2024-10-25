ed0c5e8aa219db8bf6c9b5eb666d5d4b84d5d057
Miguel Caro <miguelcaropinilla@gmail.com>
2024-10-25 13:54:23 -0500

    Add VITE_API_URL en variable for target

--- react-demo/vite.config.js
+++ react-demo/vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/proxy/api/capitolai': {
-        target: 'http://localhost:8000/api/v1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/proxy\/api\/capitolai/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = `${env.VITE_API_URL}/api/v1`;
+
+  return {
+    server: {
+      proxy: {
+        '/proxy/api/capitolai': {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/proxy\/api\/capitolai/, ''),
+        },
+      },
     },
-    },
-  },
-  plugins: [react()],
-})
+    plugins: [react()],
+  }}
+)
