60a1840367340cc92ead547fb296e87c0fc10184
Miguel Caro <miguelcaropinilla@gmail.com>
2024-10-25 17:47:28 -0500

    Update genera vercel json

--- react-demo/scripts/generate-vercel-json.js
+++ react-demo/scripts/generate-vercel-json.js
@@ -1,14 +1,16 @@
-import fs from "fs";
+// scripts/generate-vercel-json.js
+import fs from 'fs';
 
 const template = fs.readFileSync('vercel.template.json', 'utf-8');
 const VITE_API_URL = process.env.VITE_API_URL;
 
+console.log("🚀 ~ VITE_API_URL:", VITE_API_URL)
+
 if (!VITE_API_URL) {
- throw new Error('VITE_API_URL is not defined');
+  throw new Error('VITE_API_URL no está definida');
 }
 
 const content = template.replace('${VITE_API_URL}', VITE_API_URL);
 
 fs.writeFileSync('vercel.json', content);
-
-console.log(':marca_de_verificación_blanca: vercel.json generated successfully');
+console.log('✅ vercel.json generado exitosamente');
