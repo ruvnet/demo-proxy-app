3e054abc668d05db577d7cecfcc3d62cfae93de8
Miguel Caro <miguelcaropinilla@gmail.com>
2024-10-25 17:11:47 -0500

    Test prod proxy config

--- react-demo/vite.config.js
+++ react-demo/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd(), '')
   const API_URL = `${env.VITE_API_URL}/api/v1`;
+  const isDev = mode === 'development'
 
   console.log("🚀 ~ defineConfig ~ API_URL:", API_URL)
   
@@ -13,7 +14,8 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '/proxy/api/capitolai': {
           target: API_URL,
-          changeOrigin: true,
+          changeOrigin: isDev,
+          secure: !isDev,
           rewrite: (path) => path.replace(/^\/proxy\/api\/capitolai/, ''),
         },
       },
